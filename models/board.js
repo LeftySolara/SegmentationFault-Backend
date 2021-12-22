e372a3884a25c299a461f1ac107a676c31b41287
Julianne Adams <julianne@julianneadams.info>
2021-12-22 12:42:16 -0500

    Add: board model

---
+++ models/board.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const boardSchema = new Schema({
+  topic: { type: String, required: true },
+  category: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: "BoardCategory",
+  },
+  threads: {
+    type: mongoose.Types.ObjectId,
+    required: false,
+    ref: "Thread",
+  },
+});
+
+module.exports = mongoose.model(boardSchema, "Board");
