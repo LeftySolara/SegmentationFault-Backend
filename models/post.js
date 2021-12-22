f592305315132305a489ea15e4fc0fc286aab4b5
Julianne Adams <julianne@julianneadams.info>
2021-12-22 11:44:45 -0500

    Add: post model

---
+++ models/post.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+  author: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  thread: { type: mongoose.Types.ObjectId, required: true, ref: "Thread" },
+  dateCreated: { type: Date, required: true },
+  content: { type: String, required: true },
+});
+
+module.exports = mongoose.model(postSchema, "Post");
