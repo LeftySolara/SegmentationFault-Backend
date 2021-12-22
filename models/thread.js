30b80c534f86fbf3ccdb1b72a5c37e8fba1694e7
Julianne Adams <julianne@julianneadams.info>
2021-12-22 11:53:57 -0500

    Add: thread model

---
+++ models/thread.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const threadSchema = Schema({
+  author: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  topic: { type: String, required: true },
+  dateCreated: { type: Date, required: true },
+  posts: [{ type: mongoose.Types.ObjectId, required: false, ref: "Post" }],
+  lastPost: { type: mongoose.Types.ObjectId, required: false, ref: "Post" },
+});
+
+module.exports = mongoose.model(threadSchema, "Thread");
