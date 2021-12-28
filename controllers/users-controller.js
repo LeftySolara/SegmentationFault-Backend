bb72c4953e4663b430045ae4b1f255826aa661cf
Julianne Adams <julianne@julianneadams.info>
2021-12-27 20:39:13 -0500

    Add: logic for creating a new user

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,4 +1,8 @@
 const validateRequestInputs = require("../utils/inputValidator");
+const HttpError = require("../utils/http-error");
+const logger = require("../utils/logger");
+
+const User = require("../models/user");
 
 const getAllUsers = (req, res, next) => {
   return res.json({ message: "Fetching all users..." });
@@ -9,12 +13,69 @@ const getUserById = (req, res, next) => {
   return res.json({ message: `Fetching user ${id}...` });
 };
 
-const createUser = (req, res, next) => {
+/**
+ * Creates a new user and enters their info into the database.
+ *
+ * @param {String} req.body.username The username of the user.
+ * @param {String} req.body.email The user's email address.
+ * @param {String} req.body.password The user's password.
+ *
+ * @returns On success, returns a JSON-formatted HTTP response.
+ */
+const createUser = async (req, res, next) => {
   const validationError = validateRequestInputs(req);
   if (validationError) {
     return next(validationError);
   }
-  return res.json({ message: "Creating user..." });
+
+  const { username, email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ $or: [{ email }, { username }] });
+  } catch (err) {
+    const error = new HttpError("Registration failed.", 500);
+    return next(error);
+  }
+
+  if (existingUser) {
+    let error;
+    if (existingUser.email === email) {
+      error = new HttpError(
+        "Email already in use. Please log in instead.",
+        422,
+      );
+    } else if (existingUser.username === username) {
+      error = new HttpError(
+        "Username already in use. Please log in instead.",
+        422,
+      );
+    }
+    return next(error);
+  }
+
+  const createdUser = new User({
+    username,
+    email,
+    password,
+    joinDate: Date.now(),
+    avatar: "avatar",
+    posts: [],
+    threads: [],
+  });
+
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Failed to create user. Please try again.",
+      500,
+    );
+    return next(error);
+  }
+
+  logger.info(`Created new user ${username}.`);
+  return res.status(201).json({ message: `Created new user ${username}` });
 };
 
 const updateUser = (req, res, next) => {
