7a9cff291de443ce83b8d79c2461b1d6397826be
Julianne Adams <julianne@julianneadams.info>
2022-01-15 12:51:48 -0500

    Add: logic for fetching posts

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -2,23 +2,67 @@ const mongoose = require("mongoose");
 
 const validateRequestInputs = require("../utils/inputValidator");
 const HttpError = require("../utils/http-error");
+const logger = require("../utils/logger");
 
 const Post = require("../models/post");
 const Thread = require("../models/thread");
 const User = require("../models/user");
 
-const getAllPosts = (req, res, next) => {
-  return res.json({ message: "Fetching all posts..." });
+/**
+ * Fetches a list of all posts in the database.
+ */
+const getAllPosts = async (req, res, next) => {
+  let posts;
+
+  try {
+    posts = await Post.find({});
+  } catch (err) {
+    logger.error("Failed to fetch posts for GET request at /posts.");
+    const error = new HttpError("Fetching posts failed.", 500);
+    return next(error);
+  }
+
+  return res
+    .status(200)
+    .json({ posts: posts.map((post) => post.toObject({ getters: true })) });
 };
 
-const getPostById = (req, res, next) => {
-  const id = req.params.postId;
-  return res.json({ message: `Fetching post ${id}...` });
+/**
+ * Fetches a post based on its ID.
+ */
+const getPostById = async (req, res, next) => {
+  const { postId } = req.params;
+
+  let post;
+  try {
+    post = await Post.findById(postId);
+  } catch (err) {
+    const error = new HttpError(`Could not find post with ID ${postId}.`, 404);
+    return next(error);
+  }
+
+  return res.status(200).json({ post: post.toObject({ getters: true }) });
 };
 
-const getPostsByUser = (req, res, next) => {
-  const id = req.params.userId;
-  return res.json({ message: `Fetching posts by user ${id}` });
+/**
+ * Fetches a list of all posts created by a specific user.
+ *
+ * @param {String} req.params.userId The ID of the user to find posts for.
+ */
+const getPostsByUser = async (req, res, next) => {
+  const { userId } = req.params;
+
+  let posts;
+  try {
+    posts = await Post.find({ author: userId });
+  } catch (err) {
+    const error = new HttpError("Error fetching posts from user.", 500);
+    return next(error);
+  }
+
+  return res
+    .status(200)
+    .json({ posts: posts.map((post) => post.toObject({ getters: true })) });
 };
 
 /**
