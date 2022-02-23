aeba921ce4c87ed69876b197c09c72e1bd10a9a0
Julianne Adams <julianne@julianneadams.info>
2022-02-22 19:11:18 -0500

    Remove: unused route

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -12,30 +12,6 @@ router.get("/checkIsAdmin", authController.checkIsAdmin);
 router.get("/logout", authController.logout);
 
 router.post(
-  "/register",
-  [
-    check("username").not().isEmpty().isAlphanumeric(),
-    check("email").isEmail(),
-    check("password")
-      .isLength({ min: 8 })
-      .withMessage("Your password must be at least 8 characters.")
-      .matches(/\d/)
-      .withMessage("Your password should contain at least one number.")
-      .matches(/[!@#$%^&*(),.?":{}|<>]/)
-      .withMessage(
-        "Your password must contain at least one special character.",
-      ),
-    check("confirmPassword").custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Confirm password does not match.");
-      }
-      return true;
-    }),
-  ],
-  authController.registerUser,
-);
-
-router.post(
   "/login",
   check("email").isEmail(),
   check("password").not().isEmpty(),
